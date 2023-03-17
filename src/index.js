668fc0db73360ff1d3c275efde8e4ea8b2754466
marbiem <biegun.marta@gmail.com>
2023-03-17 23:34:34 +0100

    Display countries

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
 
 const searchBox = document.querySelector('input#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 const fetchCountries = async name => {
   try {
@@ -25,6 +27,12 @@ const fetchCountries = async name => {
         }
       );
     }
+    if (countries.length >= 2 && countries.length <= 10) {
+      createCountriesList(countries);
+    }
+    if (countries.length === 1) {
+      createCountryInfo(countries[0]);
+    }
     console.log('countries', countries);
   } catch (error) {
     console.log(error);
@@ -34,6 +42,35 @@ const fetchCountries = async name => {
 const onChange = event => {
   const name = event.target.value.trim();
   fetchCountries(name);
+};
+
+const createCountriesList = countries => {
+  countryList.textContent = '';
+  countries.forEach(country => {
+    const countryListElement = document.createElement('li');
+    countryListElement.append();
+    countryList.insertAdjacentHTML(
+      'beforeend',
+      `<li>
+        <img src="${country.flags.svg}" alt="${country.name.official}" height="20" />
+        ${country.name.official}
+      </li>`
+    );
+  });
+};
+
+const createCountryInfo = country => {
+  countryList.textContent = '';
+  // countryInfo.insertAdjacentHTML(
+  //   'beforeend',
+  //   `
+  //     <img src="${country.flags.svg}" alt="${country.name.official}" height="50" />
+  //     ${country.name.official}
+  //     ${country.capital}
+  //     ${country.population}
+  //     ${country.languages}
+  //   `
+  // );
 };
 
 searchBox.addEventListener('input', debounce(onChange, DEBOUNCE_DELAY));
